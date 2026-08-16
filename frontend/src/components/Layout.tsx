170db732e21630b4e8043172061aefd9f9318d0a
vrahul <vrahul@gmail.com>
2026-08-16 07:54:45 -0700

    fixes

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -14,13 +14,13 @@ export function Layout({ children }: LayoutProps) {
             <div className="flex pt-14 flex-1 min-h-0 overflow-hidden">
                 <Sidebar />
                 {/* Main container */}
-                <main className="flex-1 ml-0 md:ml-64 flex flex-col h-full min-h-0 overflow-hidden transition-all duration-300">
+                <main className="flex-1 ml-0 md:ml-64 flex flex-col h-full min-h-0 overflow-y-auto transition-all duration-300">
                     {/* Stepper container */}
                     <div className="px-3 sm:px-6 pt-2 pb-1 flex-shrink-0 bg-slate-100">
                         <PipelineStepper />
                     </div>
                     {/* Inner content container */}
-                    <div className="flex-1 flex flex-col min-h-0 px-3 sm:px-6 pb-2 overflow-hidden">
+                    <div className="flex-1 flex flex-col min-h-0 px-3 sm:px-6 pb-8">
                         {children}
                     </div>
                 </main>
